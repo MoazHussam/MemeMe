14a5569bb2a312197662e8b6974f848d1a6ae3e5
Moaz Ahmed <moaz_saad@hotmail.com>
2015-10-05 03:50:40 +0200

    build initial storyboard layout

---
+++ MemeMe/MemeEditorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemeEditorViewController.swift
+//  MemeMe
+//
+//  Created by Moaz on 10/5/15.
+//  Copyright © 2015 Moaz Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class MemeEditorViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+    }
+    
+
+}
