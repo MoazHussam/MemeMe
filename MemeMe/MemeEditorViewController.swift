7432e43d6d208e996f8d4c2c930086f7d747d825
Moaz Ahmed <moaz_saad@hotmail.com>
2015-10-05 06:48:58 +0200

    add keyboard show/hide behaviour

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController {
+class MemeEditorViewController: UIViewController, UITextFieldDelegate {
     
     //Outlets
     @IBOutlet weak var topTextField: UITextField!
@@ -16,10 +16,63 @@ class MemeEditorViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var navigationBar: UINavigationBar!
     
-    
+    //global variables
+    var isViewTransitionedUp = false    //flag to prevent multiple calls for keyboardWillShow notification
+    var keyboardHeight: CGFloat = 0.0   //to make sure height transitioned down is the same as transitioned up
     
     override func viewDidLoad() {
-        navigationBar.translucent = true
+        
+        //set text fields delegates
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        subscribeToKeyboardNotifications()
+    }
+    override func viewWillDisappear(animated: Bool) {
+        unsubscribeoKeyboardNotifications()
+    }
+    
+    /******************************* keyboard Behavior *****************/
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+
+        //dismiss the keyboard
+        textField.resignFirstResponder()
+        return false
+    }
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "transitionViewUp:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "transitionViewDown:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    func unsubscribeoKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    func transitionViewUp(notification: NSNotification) {
+        
+        //transition view up
+        if bottomTextField.isFirstResponder() && !isViewTransitionedUp {
+            updateKeyboardHeight(notification)
+            self.view.frame.origin.y -= keyboardHeight
+            isViewTransitionedUp = true
+        }
+    }
+    func transitionViewDown(notification: NSNotification) {
+
+        //re-transition view to original position
+        if isViewTransitionedUp {
+            self.view.frame.origin.y += keyboardHeight //getKeyboardHeight(notification)
+            isViewTransitionedUp = false
+            keyboardHeight = 0.0
+        }
+    }
+    func updateKeyboardHeight(notification: NSNotification) {
+
+        //update global variable keyboardHeight
+        var userinfo = notification.userInfo!
+        let keyboardSize = (userinfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue().size
+        keyboardHeight = keyboardSize.height
     }
     
     
