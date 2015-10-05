5989b2dd7418c9de900486c327a914af0c7b68bd
Moaz Ahmed <moaz_saad@hotmail.com>
2015-10-05 05:03:15 +0200

    add missing MemeViewController interface Properties add missing TextFields Properties fix status bar overlapping fix toolbar button layout

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class MemeEditorViewController: UIViewController {
     
+    //Outlets
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    
     
     override func viewDidLoad() {
+        navigationBar.translucent = true
     }
+    
+    
+    /******************************* Action Handlers **********************/
+    
+    @IBAction func shareButton(sender: UIBarButtonItem) {
+    }
+    @IBAction func cancelButton(sender: AnyObject) {
+    }
+    @IBAction func cameraButton(sender: AnyObject) {
+    }
+    @IBAction func photoAlbumButton(sender: AnyObject) {
+    }
+    
     
 
 }
